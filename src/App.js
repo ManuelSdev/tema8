09b0c2d4945874e04c661795345155024c10b761
Manuel S <masanchezzm@gmail.com>
2021-04-09 17:22:21 +0200

    usando styled component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import TweetsPage from "./components/tweets/TweetsPage.js";
-
+import Button from "./components/shared/Button";
 function App() {
   return (
     <div className="App">
+      <Button variant="primary" />
       <TweetsPage />
     </div>
   );
