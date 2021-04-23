eac388e7f96ea607ccf9c648502f2baf7302acf4
Manuel S <masanchezzm@gmail.com>
2021-04-23 09:15:58 +0200

    commit PROF Manage routes with react router dom - Creamos rutas en app.js y links en authbutton y header. Metemos BrowserRoute en nivel superior = index.js

--- src/App.js
+++ src/App.js
@@ -2,27 +2,45 @@ import React from 'react';
 import T from 'prop-types';
 import { LoginPage } from './components/auth';
 import { TweetsPage, NewTweetPage, TweetDetailPage } from './components/tweets';
+import { Switch, Route, Redirect } from 'react-router';
 
 function App({ isInitiallyLogged }) {
   const [isLogged, setIsLogged] = React.useState(isInitiallyLogged);
 
-  const handleLogin = () => setIsLogged(true);
+  const handleLogin = () => {
+    setIsLogged(true);
+  };
 
   const handleLogout = () => setIsLogged(false);
 
   return (
     <div className="App">
       <Switch>
-        <Route path="/tweet" component={NewTweetPage}></Route>
+        <Route path="/tweet/:tweetId" component={TweetDetailPage} />
+        <Route path="/tweet" component={NewTweetPage} />
+        <Route path="/login">
+          {() =>
+            isLogged ? <Redirect to="/" /> : <LoginPage onLogin={handleLogin} />
+          }
+        </Route>
+        <Route exact path="/">
+          <TweetsPage isLogged={isLogged} onLogout={handleLogout} />
+        </Route>
+        <Route path="/404">
+          <div
+            style={{
+              textAlign: 'center',
+              fontSize: 48,
+              fontWeight: 'bold',
+            }}
+          >
+            404 | Not found page
+          </div>
+        </Route>
+        <Route>
+          <Redirect to="/404" />
+        </Route>
       </Switch>
-      {isLogged ? (
-        <TweetsPage isLogged={isLogged} onLogout={handleLogout} />
-      ) : (
-        <LoginPage onLogin={handleLogin} />
-      )}
-      {/* <TweetsPage />
-      <NewTweetPage />
-      <TweetDetailPage /> */}
     </div>
   );
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
+import { Link } from 'react-router-dom';
 import { ReactComponent as Icon } from '../../assets/twitter.svg';
 import Button from '../shared/Button';
 import AuthButton from '../auth/AuthButton';
@@ -8,14 +9,14 @@ import './Header.css';
 const Header = ({ className, isLogged, onLogout, ...props }) => {
   return (
     <header className={classNames('header', className)} {...props}>
-      {/* <Link to="/"> */}
-      <div className="header-logo">
-        <Icon width="32" height="32" />
-      </div>
-      {/* </Link> */}
+      <Link to="/">
+        <div className="header-logo">
+          <Icon width="32" height="32" />
+        </div>
+      </Link>
       <nav className="header-nav">
         <Button
-          //   as={Link}
+          as={Link}
           to="/tweet"
           variant="primary"
           className="header-button"
